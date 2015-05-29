d2c5e3d4a1c26b11d39d0a4d110f22a13c46b8f4
Chad Wilken <chad.wilken@gmail.com>
2015-05-29 16:18:52 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "nginx-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@talkingquickly.co.uk"
+name              "nginx-ctw"
+maintainer        "Chad Wilken"
+maintainer_email  "me@chadwilken.com"
 description       "Installs the nginx web server from the nginx ppa"
 version           "0.0.4"
 
-recipe "nginx-tlq", "nginx server"
+recipe "nginx-ctw", "nginx server"
 
 supports "ubuntu"
